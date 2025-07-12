9b4a55a9ad67a2c900d0511c2ca49ed057ef77ba
khoa.dong <khoa.dong@aurora-tech.com>
2025-07-12 13:26:21 +0700

    update ports

--- litserve/server.py
+++ litserve/server.py
@@ -59,4 +59,4 @@ class BERTLitAPI(LitAPI):
 if __name__ == "__main__":
     api = BERTLitAPI()
     server = LitServer(api, accelerator='cpu', devices=0)
-    server.run(port=8000, num_api_servers=8)
+    server.run(port=8990, num_api_servers=8)
