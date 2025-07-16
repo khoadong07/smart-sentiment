824ff55f4097830eca0a314fb951553280448c96
khoa.dong <khoa.dong@aurora-tech.com>
2025-07-16 22:59:38 +0700

    update

--- litserve/start_socket.sh
+++ litserve/start_socket.sh
@@ -1,6 +1,6 @@
 
 # Start the server in background
-nohup uvicorn socket_server:app --host 0.0.0.0 --port 5001 --workers 16 > logs/socket_server.log 2>&1 &
+nohup uvicorn socket_server:app --host 0.0.0.0 --port 5001 --workers 8 > logs/socket_server.log 2>&1 &
 
 # Save the PID
 echo $! > socket_server.pid
