c480262ffc9fb5626d2c93f94d188a930031b56a
khoa.dong <khoa.dong@aurora-tech.com>
2025-07-17 09:21:06 +0700

    update

--- litserve/start_socket.sh
+++ litserve/start_socket.sh
@@ -1,6 +1,6 @@
 source venv/bin/activate
 
-nohup uvicorn socket_server:app --host 0.0.0.0 --port 5001 --workers 8 > logs/socket_server.log 2>&1 &
+nohup uvicorn socket_server:asgi_app --host 0.0.0.0 --port 5001 --workers 8 > logs/socket_server.log 2>&1 &
 
 # Save the PID
 echo $! > socket_server.pid
